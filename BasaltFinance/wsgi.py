3ddad6fe26aee8cfc089326cf0b6edd67d743331
amirsaleh-salehzadeh <salehzadeh@gmail.com>
2023-09-26 08:58:32 +0200

    Updated the migration and project name

--- BasaltFinance/wsgi.py
+++ BasaltFinance/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for basalt project.
+WSGI config for BasaltFinance project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basalt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BasaltFinance.settings')
 
 application = get_wsgi_application()
